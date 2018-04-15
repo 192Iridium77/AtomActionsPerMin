5cb9a2c625a9d6b1c54a76dd90fc04a7d46f6e0b
matt_martin0108 <matt@sveltestudios.com>
2018-04-15 22:04:28 +0930

    initialized apm package

---
+++ spec/actions-per-minute-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import ActionsPerMinuteView from '../lib/actions-per-minute-view';
+
+describe('ActionsPerMinuteView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
